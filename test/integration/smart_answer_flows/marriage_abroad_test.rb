a7f94e5673e14ccefdf33f2e362e35fa4a109fec
Ikenna Okpala <ikenna.okpala@digital.cabinet-office.gov.uk>
2016-04-11 12:10:10 +0100

    Content changes for opposite and same sex (Sweden)
    Content changes for opposite sex outcomes where user is resident in the UK
    
    Same and Opposite sex marriage outcomes for a resident in Sweden marrying a British/Swedish/National of another country.
    
    Redirect same sex for Sweden to OP outcome
    
    This commit reuses the outcome_opposite_sex_marriage_in_consular_cni_countries_when_residing_in_uk_or_ceremony_country
    for rendering information for same sex marriage.
    
    These changes were made upon  request from the content team.
    
    Fix failing tests for same sex in sweden

--- test/integration/smart_answer_flows/marriage_abroad_test.rb
+++ test/integration/smart_answer_flows/marriage_abroad_test.rb
@@ -1140,7 +1140,7 @@ class MarriageAbroadTest < ActiveSupport::TestCase
       add_response 'same_sex'
     end
     should "go to cp or equivalent os outcome" do
-      assert_current_node :outcome_same_sex_civil_partnership
+      assert_current_node :outcome_opposite_sex_marriage_in_consular_cni_countries_when_residing_in_uk_or_ceremony_country
     end
   end
 
